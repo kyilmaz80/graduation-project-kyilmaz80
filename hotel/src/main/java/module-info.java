eb1c908361194c11fbde4b4a002134ae5496abef
koray <kyilmaz80@gmail.com>
2023-11-06 18:02:21 +0300

    kod organizasyon

--- hotel/src/main/java/module-info.java
+++ hotel/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.kyilmaz80.hotel {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.apache.commons.dbcp2;
 
     opens com.kyilmaz80.hotel to javafx.fxml;
     exports com.kyilmaz80.hotel;
+    exports com.kyilmaz80.hotel.utils;
+    opens com.kyilmaz80.hotel.utils to javafx.fxml;
 }
