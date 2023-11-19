3ea168b929174bcda6b2cb467351335e690cf4de
koray <kyilmaz80@gmail.com>
2023-11-19 18:54:05 +0300

    reservation view change group_concat

--- hotel/src/main/resources/views.sql
+++ hotel/src/main/resources/views.sql
@@ -1,6 +1,14 @@
 CREATE VIEW `upod_otel`.`ReservationView` AS
-SELECT r.id, r.room_id, r.checkin_date, r.checkout_date,
-  r.checkedin_time, r.checkedout_time, c.full_name as customer_name
+SELECT
+    MAX(r.id) as id,
+    r.room_id,
+    MAX(r.checkin_date) AS checkin_date,
+    MAX(r.checkout_date) AS checkout_date,
+    MAX(r.checkedin_time) AS checkedin_time,
+    MAX(r.checkedout_time) AS checkedout_time,
+    GROUP_CONCAT(c.full_name) AS customer_name
   FROM `Reservation` as r
   INNER JOIN `ReservationCustomer` as rc ON r.id = rc.reservation_id
-  INNER JOIN `Customer` as c ON rc.customer_id = c.id;
+  INNER JOIN `Customer` as c ON rc.customer_id = c.id
+  GROUP BY r.room_id;
+
